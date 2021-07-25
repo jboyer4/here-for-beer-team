abae56803cb05e785f822320b1f645ac302849cb
Justin Boyer <boyerju@oregonstate.edu>
2021-07-25 15:05:54 -0700

    basic structure for yoga page

---
+++ src/app/leftPanel/leftPanel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leftPanel',
+  templateUrl: './leftPanel.component.html',
+  styleUrls: ['./leftPanel.component.css']
+})
+export class LeftPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/rightPanel/rightPanel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rightPanel',
+  templateUrl: './rightPanel.component.html',
+  styleUrls: ['./rightPanel.component.css']
+})
+export class RightPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
