6f5734a0bb741e3963c3bdf65506702b0b95f3a8
Justin Boyer <boyerju@oregonstate.edu>
2021-08-01 22:10:57 -0700

    Add text as an alert

--- src/app/rightPanel/rightPanel.component.ts
+++ src/app/rightPanel/rightPanel.component.ts
@@ -11,5 +11,7 @@ export class RightPanelComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  showDetails(){
+    alert("The pose has the head down, ultimately touching the floor, with the weight of the body on the palms and the feet. The arms are stretched straight forward, shoulder width apart; the feet are a foot apart, the legs are straight, and the hips are raised as high as possible");
+  }
 }
